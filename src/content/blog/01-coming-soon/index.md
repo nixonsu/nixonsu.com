00e073fd91c794ba41a80377fcb8bef0b37a275f
nixonsu <nixon.s.uch@gmail.com>
2025-08-14 23:08:47 +1000

    chore: remove blogs

--- src/content/blog/01-coming-soon/index.md
+++
@@ -1,5 +0,0 @@
----
-title: "Coming soon :)"
-description: ""
-date: "Dec 19 2024"
----
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,5 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-  }),
-});
-
 const projects = defineCollection({
   type: "content",
   schema: z.object({
@@ -22,4 +12,4 @@ const projects = defineCollection({
   }),
 });
 
-export const collections = { blog, projects };
+export const collections = { projects };
